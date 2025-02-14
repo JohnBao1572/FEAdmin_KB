e015e44c1ea616be78d51cc199ce7c48c0cef023
Bao <jonnguyen1572@gmail.com>
2025-02-14 15:57:23 +0700

    Receive new order notificaiton and update notification from Customer just ordered

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,18 +1,23 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { authSelector, removeAuth } from '../reduxs/reducers/authReducer'
-import { useNavigate } from 'react-router-dom';
-import { Avatar, Button, Dropdown, Input, MenuProps, Space } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import { Avatar, Badge, Button, Drawer, Dropdown, Input, List, MenuProps, Space, Typography } from 'antd';
 import { auth } from '../firebase/firebaseConfig';
 import { Auth } from 'firebase/auth';
 import { signOut } from 'firebase/auth';
 import { SearchNormal1 } from 'iconsax-react';
 import { IoIosNotifications } from "react-icons/io";
 import { colors } from '../constants/colors';
+import handleAPI from '../apis/handleAPI';
+import { NotificationModel } from '../models/NotificationModel';
+import { DateTime } from '../utils/dateTime';
 
 
 const HeaderComponent = () => {
+    const [notificaitons, setNotiifications] = useState<NotificationModel[]>([]);
+    const [visibleModalNoti, setVisibleModalNoti] = useState(false);
 
     const user = useSelector(authSelector);
     const dispatch = useDispatch();
@@ -33,6 +38,41 @@ const HeaderComponent = () => {
         },
     ];
 
+
+    useEffect(() => {
+        getNotifications();
+    }, [])
+    const getNotifications = async () => {
+        const api = `/notifications`;
+        // console.log(api);
+        try {
+            const res = await handleAPI(api);
+            // console.log(res);
+
+            setNotiifications(res.data);
+        } catch (error: any) {
+            console.log(error.message);
+        }
+    }
+    // console.log(notificaitons);
+
+    const handleReadNotification = async (item: NotificationModel) => {
+        if (!item.isRead) {
+            const api = `/notifications/updateNewOrderNoti?id=${item._id}`;
+
+            try {
+                const res = await handleAPI(api, { isRead: true }, 'put')
+                await getNotifications();
+            } catch (error: any) {
+                console.log(error.message);
+            }
+        }
+
+        // console.log(item)
+        setVisibleModalNoti(false);
+        navigate(`/order?id=${item._id}`)
+    }
+
     return (
         <div className='p-2 row bg-white m-0'>
             <div className="col">
@@ -49,8 +89,16 @@ const HeaderComponent = () => {
 
             <div className="col text-right">
                 <Space>
-                    <Button type='text'
-                        icon={<IoIosNotifications size={22} color={colors.gray600} />}
+                    <Button
+                        onClick={() => setVisibleModalNoti(true)}
+                        type='text'
+                        icon={
+                            <Badge count={
+                                notificaitons.filter((element) => !element.isRead).length
+                            }>
+                                <IoIosNotifications size={22} color={colors.gray600} />
+                            </Badge>
+                        }
                     />
 
                     <Dropdown menu={{ items }}>
@@ -58,6 +106,37 @@ const HeaderComponent = () => {
                     </Dropdown>
                 </Space>
             </div>
+
+            <Drawer open={visibleModalNoti}
+                onClose={() => setVisibleModalNoti(false)}>
+                <List
+                    dataSource={notificaitons}
+                    renderItem={(item) => (
+                        <List.Item key={item._id}
+                            // onClick={item.isRead ? undefined : () => { handleReadNotification(item) }}
+                            >
+                            <List.Item.Meta
+                                title={
+                                    <Button
+                                    type='link'
+                                    onClick={() => handleReadNotification(item)}>
+                                        <Typography.Text className={item.isRead ? 'text-muted' : ''}>
+                                            {item.title}
+                                        </Typography.Text>
+                                    </Button>
+                                }
+                                description={item.body}
+                            />
+
+                            <Typography.Text type='secondary'>
+                                {DateTime.CalendarDate(item.createdAt)}
+                            </Typography.Text>
+
+                        </List.Item>
+                    )}>
+
+                </List>
+            </Drawer>
         </div>
     )
 }
---
+++ src/models/NotificationModel.ts
@@ -0,0 +1,14 @@
+export interface NotificationModel {
+	_id: string;
+	title: string;
+	body: string;
+	id: string;
+	url: string;
+	isRead: boolean;
+	fromId: string;
+	toId: string;
+	to: string;
+	createdAt: string;
+	updatedAt: string;
+	__v: number;
+}
